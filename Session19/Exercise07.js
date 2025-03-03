e67d03889ccbb473c44c766298e311db21564619
unknown <namuy24@gmail.com>
2025-03-03 16:10:55 +0700

    session19

--- Session19/Exercise07.js
+++ Session19/Exercise07.js
@@ -4,7 +4,7 @@ let num3 = +prompt("Nhập số thứ ba: ");
 let max;
 // let max = Math.max(num1,num2,num3);
 if (Number.isInteger(num1) && Number.isInteger(num2) && Number.isInteger(num3)) {
-    num1 > num2 && num1 > num3 ? max = num1 : num2>num3 ? max= num2 : max = num3;
+    (num1 > num2 && num1 > num3) ? (max = num1) : (num2>num3) ? (max = num2) : (max = num3);
 } else {
     alert("Vui lòng nhập vào số")
 }
--- Session19/Exercise08.js
+++ Session19/Exercise08.js
@@ -1,18 +1,12 @@
 let month = +prompt("Nhập tháng để kiếm tra");
-switch (month) {
-    case 1,2,3:
-        alert("Mùa Xuân");
-        break;
-    case 4,5,6:
-        alert("Mùa Hạ");
-        break;
-    case 7,8,9:
-        alert("Mùa Thu");
-        break;
-    case 10,11,12:
-        alert("Mùa Đông");
-        break;
-    default:
-        alert("Tháng không hợp lệ");
-        break;
+if (month === 1 || month === 2 || month===3) {
+    alert("Mùa xuân");
+} else if(month === 4 || month === 5 || month===6) {
+    alert("Mùa hè");
+} else if (month === 7 || month === 8 || month===9) {
+    alert("Mùa thu");
+} else if(month === 10 || month === 11 || month===12) {
+    alert("Mùa đông");
+} else {
+    alert("Tháng nhập vào không hợp lệ");
 }
