fee91f73ef9fd863948c4f157eb0eb2fd43813bd
unknown <namuy24@gmail.com>
2025-03-03 15:01:57 +0700

    update

--- Session19/Exercise04.js
+++ Session19/Exercise04.js
@@ -1,6 +1,10 @@
 let input = +prompt("Nhập số để kiểm tra");
-if (input%3===0 && input%5===0) {
-    alert(`${input} chia hết cho cả 3 và 5`);
-} else {
-    alert(`${input} không chia hết cho cả 3 và 5`);
+if (Number.isInteger(input)) {
+    if (input%3===0 && input%5===0) {
+        alert(`${input} chia hết cho cả 3 và 5`);
+    } else {
+        alert(`${input} không chia hết cho cả 3 và 5`);
+    }
+} else{
+    alert("Vui Lòng nhập vào 1 số")
 }
