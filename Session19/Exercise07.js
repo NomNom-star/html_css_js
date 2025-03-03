46e5b3d23888a7672369a399d698d4ebf8ff5d8c
unknown <namuy24@gmail.com>
2025-03-03 15:12:34 +0700

    session19

--- Session19/Exercise07.js
+++ Session19/Exercise07.js
@@ -3,5 +3,10 @@ let num2 = +prompt("Nhập số thứ hai: ");
 let num3 = +prompt("Nhập số thứ ba: ");
 let max;
 // let max = Math.max(num1,num2,num3);
-num1 > num2 && num1 > num3 ? max= num1 : num2>num3 ? max= num2 : max = num3;
+if (Number.isInteger(num1) && Number.isInteger(num2) && Number.isInteger(num3)) {
+    num1 > num2 && num1 > num3 ? max = num1 : num2>num3 ? max= num2 : max = num3;
+} else {
+    alert("Vui lòng nhập vào số")
+}
+
 alert(` Số lớn nhất trong 3 số là: ${max}`);
